77028ad6ef32f7928dfb2aa007e68725d963c519
Almgohar <Almgohar@almgohars-mbp.lan>
2014-11-28 16:40:27 +0200

    Add walls colors

--- main.cpp
+++ main.cpp
@@ -19,15 +19,222 @@ bool start;
 int windowWidth = 680;
 int windowHeight = 350;
 float z = -5.0f;
-int left[10][50];
-int right[10][50];
-int top[20][50];
-int bottom[20][50];
-void generateRandom(void);
+int left[12][48] = {{1,1,1,2,2,2,3,3,3,4,4,4,
+    1,1,1,2,2,2,3,3,3,4,4,4,
+    1,1,1,2,2,2,3,3,3,4,4,4,
+    1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3}};
+
+int right[12][48] = {{2,2,2,3,3,3,4,4,4,1,1,1,
+    2,2,2,3,3,3,4,4,4,1,1,1,
+    2,2,2,3,3,3,4,4,4,1,1,1,
+    2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4}};
+
+int top[12][48] = {{3,3,3,4,4,4,1,1,1,2,2,2,
+    3,3,3,4,4,4,1,1,1,2,2,2,
+    3,3,3,4,4,4,1,1,1,2,2,2,
+    3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1}};
+
+int bottom[12][48] = {{4,4,4,1,1,1,2,2,2,3,3,3,
+    4,4,4,1,1,1,2,2,2,3,3,3,
+    4,4,4,1,1,1,2,2,2,3,3,3,
+    4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    {4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3,
+        4,4,4,1,1,1,2,2,2,3,3,3},
+    
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    {1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4,
+        1,1,1,2,2,2,3,3,3,4,4,4},
+    
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    {2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1,
+        2,2,2,3,3,3,4,4,4,1,1,1},
+    
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2},
+    {3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2,
+        3,3,3,4,4,4,1,1,1,2,2,2}};
+
 void drawLeftWall(void);
-double sphX;
-double sphY;
-double sphZ;
+void drawRightWall(void);
+void drawTopWall(void);
+void drawBottomWall(void);
+void sphere(void);
+double sphX = 6;
+double sphY = 3;
+double sphZ = -3;
 /* Initialize OpenGL Graphics */
 void initGL() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
@@ -49,32 +256,133 @@ void sphere(){
 }
 
 void drawLeftWall(){
-	for(int i = 0; i < 8; i++) {
-		for(int j = 0; j < 4; j++) {
-			srand(time(NULL));
-			generateRandom();
-			glVertex3f(-6.0f,  (float)j+3, -(float)i-6);
-			glVertex3f(-6.0f, (float)j+3, (float)i);
-			glVertex3f(-6.0f, (float)j,  (float)i);
-			glVertex3f(-6.0f,  (float)j,  -(float)i-6);
-			
-		}
-	}
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 48; j++) {
+            switch (left[i][j]) {
+                case 1:
+                    glColor3f(0.6f, 0.6f, 1.0f);
+                    break;
+                    
+                case 2:
+                    glColor3f(0.0f, 0.8f, 0.8f);
+                    break;
+                    
+                case 3:
+                    glColor3f(0.4f, 0.0f, 0.4f);
+                    break;
+                    
+                case 4:
+                    glColor3f(0.0f, 0.5f, 1);
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            glVertex3f(0.0f,  (float)i+1, -((float)j+1));
+            glVertex3f(0.0f, (float)i+1, -j);
+            glVertex3f(0.0f, i,  -j);
+            glVertex3f(0.0f,  i,  -((float)j+1));
+        }
+    }
 }
 
-void generateRandom(){
-	int color = rand()%4 + 1;
-	switch(color) {
-		case 1: glColor3f(0.6f, 0.6f, 1.0f); break;
-		case 2: glColor3f(0.0f, 0.8f, 0.8f); break;
-		case 3: glColor3f(0.4f, 0.0f, 0.4f); break;
-		case 4: glColor3f(0.0f, 0.5f, 1); break;
-		default: glColor3f(0.0f,0.0f,0.0f); break;
-	}
+void drawRightWall(){
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 48; j++) {
+            switch (right[i][j]) {
+                case 1:
+                    glColor3f(0.6f, 0.6f, 1.0f);
+                    break;
+                    
+                case 2:
+                    glColor3f(0.0f, 0.8f, 0.8f);
+                    break;
+                    
+                case 3:
+                    glColor3f(0.4f, 0.0f, 0.4f);
+                    break;
+                    
+                case 4:
+                    glColor3f(0.0f, 0.5f, 1);
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            glVertex3f(12.0f,  (float)i+1, -((float)j+1));
+            glVertex3f(12.0f, (float)i+1, -j);
+            glVertex3f(12.0f, i,  -j);
+            glVertex3f(12.0f,  i,  -((float)j+1));
+        }
+    }
 }
 
-	
+void drawTopWall(){
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 48; j++) {
+            switch (top[i][j]) {
+                case 1:
+                    glColor3f(0.6f, 0.6f, 1.0f);
+                    break;
+                    
+                case 2:
+                    glColor3f(0.0f, 0.8f, 0.8f);
+                    break;
+                    
+                case 3:
+                    glColor3f(0.4f, 0.0f, 0.4f);
+                    break;
+                    
+                case 4:
+                    glColor3f(0.0f, 0.5f, 1);
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            
+            glVertex3f(i+1, 12.0f, -j);
+            glVertex3f(i, 12.0f, -j);
+            glVertex3f(i, 12.0f,  -(j+1));
+            glVertex3f(i+1, 12.0f,  -(j+1));
+        }
+    }
+}
 
+void drawBottomWall(){
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 48; j++) {
+            switch (bottom[i][j]) {
+                case 1:
+                    glColor3f(0.6f, 0.6f, 1.0f);
+                    break;
+                    
+                case 2:
+                    glColor3f(0.0f, 0.8f, 0.8f);
+                    break;
+                    
+                case 3:
+                    glColor3f(0.4f, 0.0f, 0.4f);
+                    break;
+                    
+                case 4:
+                    glColor3f(0.0f, 0.5f, 1);
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            glVertex3f(i+1, 0.0f, -j);
+            glVertex3f(i, 0.0f, -j);
+            glVertex3f(i, 0.0f,  -(j+1));
+            glVertex3f(i+1, 0.0f,  -(j+1));
+        }
+    }
+}
 
 /* Handler for window-repaint event. Called back when the window first appears and
  whenever the window needs to be re-painted. */
@@ -101,51 +409,25 @@ void display() {
 	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
 	
 	glLoadIdentity();// Reset the model-view matrix
-	gluLookAt(0.0, 6.0, 0.1, 0, 6.0, 0, 0.0, 12.0, 0.0);
-	glTranslatef(0.5f, 0.0f, 0.0f);  // Move right and into the screen
+    gluLookAt(6.0, 6.0, 0.1, 6.0, 6.0, 0, 0.0, 12.0, 0.0);
 	
 	
 	
-	glBegin(GL_QUADS);                // Begin drawing the room
-	// Top face (y = 12.0f)
-	// Define vertices in counter-clockwise (CCW) order with normal pointing out
-	glColor3f(0.0f, 0.5f, 1.0f);
-	glVertex3f( 6.0f, 12.0f, 0.0f);
-	glVertex3f(-6.0f, 12.0f, 0.0f);
-	glVertex3f(-6.0f, 12.0f,  -48.0f);
-	glVertex3f( 6.0f, 12.0f,  -48.0f);
-	
-//     // Back wall (z = -48.0f)
-	glColor3f(1.0f,0.2f, 0.0f);
-	glVertex3f( 6.0f, 12.0f,  -48.0f);
-	glVertex3f(-6.0f, 12.0f,  -48.0f);
-	glVertex3f(-6.0f, 0.0f, -48.0f);
-	glVertex3f( 6.0f, 0.0f, -48.0f);
-	
-//     // bottom wall (y = 0.0f)
-	glColor3f(0.0, 0.5f, 1.0f);
-	glVertex3f( 6.0f, 0.0f, 0.0f);
-	glVertex3f(-6.0f, 0.0f, 0.0f);
-	glVertex3f(-6.0f,  0.0f, -48.0f);
-	glVertex3f( 6.0f,  0.0f, -48.0f);
-	
-// //    // Left wall (x = -6.0f)
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glVertex3f(-6.0f,  12.0f, -48.0f);
-	glVertex3f(-6.0f, 12.0f, 0.0f);
-	glVertex3f(-6.0f, 0.0f,  0.0f);
-	glVertex3f(-6.0f,  0.0f,  -48.0f);
-	
-   // drawLeftWall();
-	
-//     // Right wall (x = 6.0f)
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glVertex3f(6.0f, 12.0f, 0.0f);
-	glVertex3f(6.0f, 12.0f, -48.0f);
-	glVertex3f(6.0f, 0.0f,  -48.0f);
-	glVertex3f(6.0f, 0.0f, 0.0f);
-	glEnd();  // End of drawing color-cube
-	
+    glBegin(GL_QUADS);                // Begin drawing the room
+    // Back wall (z = -48.0f)
+    glColor3f(1.0f,0.2f, 0.0f);
+    glVertex3f( 12.0f, 12.0f,  -48.0f);
+    glVertex3f(0.0f, 12.0f,  -48.0f);
+    glVertex3f(0.0f, 0.0f, -48.0f);
+    glVertex3f(12.0f, 0.0f, -48.0f);
+    
+    drawTopWall();
+    drawBottomWall();
+    drawLeftWall();
+    drawRightWall();
+    
+    glEnd();  // End of drawing room
+    
 	sphere(); //ersem el sphere
 
 	glFlush();
@@ -156,19 +438,19 @@ void display() {
 
 void SetupLights()
 {
-	GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7, 1.0f };
-	GLfloat mat_diffuse[] = { 0.6f, 0.6f, 0.6, 1.0f };
-	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
-	GLfloat mat_shininess[] = { 50 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	//set the light source properties
-	GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
-	GLfloat light_position[] = { -7.0f, 6.0f, 3.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+    GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7, 1.0f };
+    GLfloat mat_diffuse[] = { 0.6f, 0.6f, 0.6, 1.0f };
+    GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
+    GLfloat mat_shininess[] = { 90 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    //set the light source properties
+    GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
+    GLfloat light_position[] = { -10.0f, 12.0f, 0.1f, 1.0f };
+    glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 }
 
 /* Handler for window re-size event. Called back when the window first appears and
@@ -186,19 +468,18 @@ void reshape(GLsizei width, GLsizei height) {
 }
 
 void anim(void) {
-	for(int i=0;i<100000000;i++);
-	SetupLights();
-		glViewport(0, 0, windowWidth, windowHeight);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	if(angle > 20 && start){
-		angle-=3;
-		z-= 1.0f;
-		gluPerspective(angle, (float)windowWidth/(float)windowHeight, 0.1f, 50.0f);
-	glutPostRedisplay();
-	}
-
-}
+    for(int i=0;i<100000000;i++);
+    SetupLights();
+    glViewport(0, 0, windowWidth, windowHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(angle > 20 && start && sphZ > -48){
+        angle-=3;
+        sphZ-= 1;
+        gluPerspective(angle, (float)windowWidth/(float)windowHeight, 0.1f, 50.0f);
+        glutPostRedisplay();
+    }
+    
 
 void Keyboard(unsigned char key, int x, int y)
 {
